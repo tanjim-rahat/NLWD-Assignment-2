d3ffd5b2661477d23e330df3311f973bebdebd0b
Tanjim Rahat <tanjim.rahat.dipto@gmail.com>
2026-05-22 23:41:49 +0600

    Basic setup for pg and neondb database

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,44 @@
-import express from "express";
+import express, {
+  type Application,
+  type Request,
+  type Response,
+} from "express";
+import { Pool } from "pg";
+import AuthRouter from "./routes/auth";
 
-const app = express();
+const app: Application = express();
 const PORT = process.env.PORT || 3000;
 
-app.get("/", (req, res) => {
+const pool = new Pool({
+  connectionString:
+    process.env.DATABASE_URL ||
+    "postgresql://neondb_owner:npg_NgB6m7zfLjSE@ep-blue-bar-aobyy6w5.c-2.ap-southeast-1.aws.neon.tech/neondb?sslmode=verify-full",
+});
+
+const initDB = async () => {
+  try {
+    await pool.query(`
+      CREATE TABLE IF NOT EXISTS users (
+        id SERIAL PRIMARY KEY,
+        name VARCHAR(255) NOT NULL,
+        email VARCHAR(255) NOT NULL UNIQUE,
+        password VARCHAR(255) NOT NULL,
+        role VARCHAR(50) NOT NULL,
+        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+        updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+      );
+    `);
+    console.log("Database initialized successfully!");
+  } catch (err) {
+    console.error("Error initializing database:", err);
+  }
+};
+
+initDB();
+
+app.use("/api/auth", AuthRouter);
+
+app.get("/", (req: Request, res: Response) => {
   res.send("Hello, World!");
 });
 
